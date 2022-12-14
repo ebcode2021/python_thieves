d38bcfdc3bf712a22e7e23cba07cda496b9a55f7
ebcode2021 <ebcode2021@gmail.com>
2022-12-14 16:44:43 +0900

    solve :: modify solve

--- eunbi/Part02/greedy/3.숫자 카드 게임.py
+++ eunbi/Part02/greedy/3.숫자 카드 게임.py
@@ -1,10 +1,9 @@
 rows, cols = map(int, input().split())
 
-max = 0
+res = 0
 
 for _ in range(rows) :
 	tmp_list = list(map(int, input().split()))
-	if (max < min(tmp_list)) :
-		max = min(tmp_list)
+	res = max(res, min(tmp_list))
 
-print(max)
+print(res)
